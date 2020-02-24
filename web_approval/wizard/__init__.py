7e35d0503b9b099b8ca514c605163e2d9f6016b4
粟雪峰 <5392510@qq.com>
2020-02-24 13:30:02 +0800

    update

--- web_approval/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import approval_wizard
-from . import dispatch_approval_user_wizard
-from . import approval_turn_to_wizard
-from . import add_node_action_wizard
-from . import approval_increase_wizard
-
-
